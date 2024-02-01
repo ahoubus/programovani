f7ba1dc3c845d46780370307e3854839bdff28c1
ahoubus <svobodadominik@gmail.com>
2024-02-01 17:04:33 +0100

    BBBBB

--- mau/main.js
+++ mau/main.js
@@ -4,7 +4,7 @@ class Circle {
 
     constructor(x, y, siz, colr) {
         this.pos = createVector(x, y)
-        this.vel = createVector();
+        this.vel = createVector(random(30), 0);
         this.size = siz
         this.colour = colr
     }
@@ -17,7 +17,9 @@ class Circle {
 
 
     applyForce(f) {
-        this.vel.add(f);
+        let scaledForce = f.copy()
+        scaledForce.div(this.size / 50)
+        this.vel.add(scaledForce);
     }
 
 
@@ -32,7 +34,21 @@ class Circle {
             this.pos.y = height - 10;
             this.vel.y = -this.vel.y*0.98;
         }
-        
+        if (this.pos.x > width - 10) {
+            this.pos.x = width - 10;
+            this.vel.x = -this.vel.x*0.98;
+        }
+        if (this.pos.x < 0 + 10) {
+            this.pos.x = 0 + 10;
+            this.vel.x = this.vel.x*-0.88;
+        }
+
+        stroke (255)
+        line (850, 200, 1100, 200)
+        line (1100, 200, 1100, 350)
+        line (1100, 350, 850, 350)
+        line (850, 350, 850, 200)
+        noStroke();
     }
 }
 
@@ -56,4 +72,23 @@ function draw() {
     background(0)
     for (let circle of circlez) circle.update();
     for (let circle of circlez) circle.draw();
+
+    keyCode = 32
+    if (keyIsPressed) {
+        gravity.y = -0.5
+    }    
+    if (gravity.y == -0.5){
+        gravity.y = gravity.y-0.01
+    }
+    
+    if (!keyIsPressed) {
+        gravity.y = 0.5
+    }
 }
+
+//function keyPressed() {
+ //   if (keyCode == 32)
+//        gravity.y = -gravity.y
+//    if (keyCode == 83)
+//       gravity.y = gravity.y/2
+//}
